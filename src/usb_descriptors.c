3d907f5fbb101c29da952277739ffe13084acb24
Stavros Korokithakis <hi@stavros.io>
2025-06-19 04:04:23 +0300

    Change the USB descriptor

--- src/usb_descriptors.c
+++ src/usb_descriptors.c
@@ -172,8 +172,8 @@ uint8_t const *tud_descriptor_configuration_cb(uint8_t index)
 char const *string_desc_arr[] =
     {
         (const char[]){0x09, 0x04}, // 0: is supported language is English (0x0409)
-        "rktrlng",                  // 1: Manufacturer
-        "TinyUSB keyboard",         // 2: Product
+        "Stavros",                  // 1: Manufacturer
+        "Dialogue",                 // 2: Product
         "123456",                   // 3: Serials, should use chip ID
 };
 
